269d292e68cc7e371c5052e46e4dc86d656cce86
elianamsf <elianamsfcontatos@gmail.com>
2018-12-12 03:23:03 -0300

    Templetes (Layout inicial)

--- app/controllers/rotas.py
+++ app/controllers/rotas.py
@@ -1,10 +1,17 @@
 from app import app
 import pymysql
+from  flask import render_template
 
 from pymysql import cursors
 @app.route("/")
 def index():
-    return ("Pagina Inicial, use /v para ver o banco")
+    #return ("Pagina Inicial, use /v para ver o banco")
+    return render_template('index.html')
+
+@app.route("/index")
+def index2():
+    #return ("Pagina Inicial, use /v para ver o banco")
+    return render_template('index.html')
 
 @app.route("/v")
 def bdView():
@@ -24,4 +31,31 @@ def bdView():
 
 @app.route("/m")
 def mapView():
-    return ("Pagina do mapinha")
+    # return ("Pagina do mapinha")
+    return render_template('mapa.html')
+
+
+@app.route("/g")
+def chartsView():
+    return render_template('grafico.html')
+
+
+@app.route("/r")
+def reportView():
+    return render_template('relatorio.html')
+
+@app.route("/contato")
+def contatView():
+    return render_template('contato.html')
+
+@app.route("/sobre")
+def aboutView():
+    return render_template('sobre.html')
+@app.route("/saiba_mais")
+
+def moreView():
+    return render_template('saiba_mais.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
